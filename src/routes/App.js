fa014f59b3657d5cbb4eaaec2d0f309ffaca1168
Nouaman <89440867+Noulamin@users.noreply.github.com>
2022-12-30 17:44:13 -0800

    lol

--- src/routes/App.js
+++ src/routes/App.js
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/lol" element={<LOL/>} />
+        <Route path="lol" element={<LOL/>} />
       </Routes>
     </BrowserRouter>
   );
