735180ceed808ddf7fe58c9ea715a65b46fcd058
Nouaman <89440867+Noulamin@users.noreply.github.com>
2022-12-30 16:25:03 -0800

    Initial project

---
+++ src/containers/Home/lol.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function lol() {
+
+  return (
+    <>
+      Woahhh Click here :
+      <button>
+        AAAAAAAAAAAAAAAAA
+      </button>
+    </>
+  );
+}
+
+export default lol;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../containers/Home/index";
+import LOL from "../containers/Home/lol";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/lol" element={<LOL/>} />
       </Routes>
     </BrowserRouter>
   );
